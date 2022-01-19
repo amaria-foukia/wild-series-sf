35c38dba3e9f82bbf4fb88ba1433db622f5d26f4
amaria-foukia <amaria.foukia@gmail.com>
2022-01-19 10:57:22 +0100

    quete 9 : ajout de fixtures

---
+++ src/DataFixtures/CategoryFixtures.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+
+class CategoryFixtures extends Fixture
+{
+    const CATEGORIES = [
+        'Action',
+        'Aventure',
+        'Animation',
+        'Fantastique',
+        'Horreur'
+    ];
+
+    /**
+     * @inheritDoc
+     */
+    public function load(ObjectManager $manager)
+    {
+        foreach (self::CATEGORIES as $key => $categoryName){
+            $category = new Category();
+            $category->setName($categoryName);
+
+            $manager->persist($category);
+        }
+        $manager->flush();
+    }
+}
